50347588930448b85a16981c64592814bd0444ee
Nikolai V. Fredriksen <cooprix1@gmail.com>
2025-07-24 16:49:18 +0200

    ComputerCanvas visually showing

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
 const Computers = () => {
-  return <div>Computers</div>;
+  const computer = useGLTF("/desktop_pc/scene.gltf");
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+        scale={2.5}
+        position-y={0}
+        rotation-y={Math.PI / 4}
+      />
+    </mesh>
+  );
 };
 
-export default Computers;
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
