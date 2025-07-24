00f5b14160eb8826f265fd80d3fb5f98070c9e33
Nikolai V. Fredriksen <cooprix1@gmail.com>
2025-07-24 21:38:55 +0200

    Started on Experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { textVariant } from "../utils/motion";
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I've done so far</p>
+        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+      </motion.div>
+    </>
+  );
+};
 
-export default Experience
+export default Experience;
