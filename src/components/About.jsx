9fd0fe6e97d290aceb21dcc90f31eb3b44746ade
Nikolai V. Fredriksen <cooprix1@gmail.com>
2025-07-24 18:48:07 +0200

    Started About-page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+    </>
+  );
+};
 
-export default About
+export default About;
