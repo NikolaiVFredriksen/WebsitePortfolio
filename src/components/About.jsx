8c3d7d10902ccb61e8c66450a701251138dae4ae
Nikolai V. Fredriksen <cooprix1@gmail.com>
2025-07-24 20:00:40 +0200

    Added About-Cards

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,60 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
+import { textVariant, fadeIn } from "../utils/motion";
+import { services } from "../constants";
+import { Tilt } from "react-tilt";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", index * 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
     <>
-      <motion.div>
+      <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I am a software developer with a passion for creating innovative
+        solutions. My journey in software development has been driven by a
+        desire to solve real-world problems and enhance user experiences. I have
+        experience in various programming languages and frameworks, and I am
+        always eager to learn new technologies. My goal is to contribute to
+        projects that make a positive impact on people's lives.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   );
 };
