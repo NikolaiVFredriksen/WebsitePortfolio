9c2c70fd08455d2dba5dc17cfac03e533c664b0d
Nikolai V. Fredriksen <cooprix1@gmail.com>
2025-09-03 22:13:22 +0200

    New hero, largely improved

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,36 +5,31 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className="relative w-full h-screen mx-auto ">
       <div
-        className={`padding-x absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`padding-x absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 !mt-10`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          <div className="w-5 h-5 rounded-full bg-[#8B3A3A]" />
+          <div className="w-1 sm:h-80 h-40 burgundy-gradient" />
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hei, <span className="text-[#915eff]">Nikolai</span> her
+          <h1 className={`${styles.heroHeadText} !text-black-100 `}>
+            Hei, <span className="text-[#8B3A3A]">Nikolai</span> her
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          <p className={`${styles.heroSubText} mt-2 text-black-100 w-3/4`}>
             Fullstack-utvikler med fokus på god brukeropplevelse og pålitelig
             kode
           </p>
         </div>
       </div>
 
-      <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
+      <div className="absolute bottom-0 left-0 flex items-end justify-end w-full h-auto ">
         <img
-          src="/headshot.jpg"
+          src="/headshot.png"
           alt="headshot"
-          className="
-    w-[300px] h-[300px]
-    sm:w-[350px] sm:h-[350px]
-    md:w-[400px] md:h-[400px]
-    rounded-full object-cover shadow-xl ring-2 ring-white/10
-  "
+          className="2xl:w-[1300px] xl:w-[1000px] lg:w-[800px] md:w-[700px] sm:w-[700px] xs:w-[700px] w-[150px] max-w-[1300px] h-auto rounded-4xl object-cover transition-all duration-300 ease-in-out"
         />
       </div>
 
