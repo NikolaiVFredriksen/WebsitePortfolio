6775859fb5be6f5eb5375f6d1b75b03369ef2bd9
Nikolai V. Fredriksen <cooprix1@gmail.com>
2025-09-09 19:02:20 +0200

    Small changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
       <footer className="w-full flex justify-center items-center flex-col p-4 bg-tertiary !pt-3 justify-center">
-        <p className="text-white text-sm">
+        <p className="text-white text-sm !pb-1">
           © 2025 Nikolai Villanueva Fredriksen
         </p>
       </footer>
